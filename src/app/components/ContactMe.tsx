51583195c38b50ad59e81914efe3832cb41011c3
Durgeshityar <dchandrakar2909@gmail.com>
2024-05-06 21:19:06 +0530

    added svg

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -1,3 +1,5 @@
+import { GithubSvg, LinkdinSvg, TwitterSvg } from './footer'
+
 const ContactMe = () => {
   return (
     <div className="p-8  flex flex-col gap-4">
@@ -7,21 +9,27 @@ const ContactMe = () => {
           href="https://shorturl.at/acvy1"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex items-center gap-1"
         >
+          <LinkdinSvg />
           <li className="cursor-pointer">Linkedin</li>
         </a>
         <a
           href="https://twitter.com/Durgesh_47"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex items-center gap-1"
         >
+          <TwitterSvg />
           <li className="cursor-pointer">Twitter</li>
         </a>
         <a
           href=" https://github.com/Durgeshityar"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex items-center gap-1"
         >
+          <GithubSvg />
           <li className="cursor-pointer"> Github</li>
         </a>
       </ul>
